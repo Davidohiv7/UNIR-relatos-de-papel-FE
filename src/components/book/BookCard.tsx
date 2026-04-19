91e740ea45ecf0c00ae008567296f4c113528fc4
Davidohiv7 <davidbu7@hotmail.com>
2026-04-19 17:18:47 -0500

    feat: create React + TS + React router boilerplate

---
+++ src/components/book/BookCard.tsx
@@ -0,0 +1,22 @@
+import { Button } from "../ui";
+
+type Props = {
+  title: string;
+  author: string;
+  price: number;
+  image: string;
+};
+
+function BookCard({ title, author, price, image }: Props) {
+  return (
+    <div>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <p>{price}</p>
+      <Button label="Add to Cart" onClick={() => {}} />
+    </div>
+  );
+}
+
+export default BookCard;
