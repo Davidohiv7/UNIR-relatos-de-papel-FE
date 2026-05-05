bb52cc9d621f6aa258c18e46adba9ac7b1e93682
Davidohiv7 <davidbu7@hotmail.com>
2026-05-05 16:01:10 -0500

    fix: remove missing file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,5 @@
 import BrandIcon from './BrandIcon';
 import Button from './Button';
 import CustomerAvatar from './CustomerAvatar';
-import Examples from './Examples';
 
-export { BrandIcon, Button, CustomerAvatar, Examples };
+export { BrandIcon, Button, CustomerAvatar };
